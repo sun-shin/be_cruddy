880859f2600b812ef53c690055c04b653e665b62
sun-shin <sunhanshin2@EMAIL.com>
2020-09-11 08:38:12 -0500

    new instances added to seeds.rb and movies controller created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
 actor.save
+
+my_movie = Movie.create({title: "Fight Club", year:"1999", plot: "An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more."})
+
+my_movie = Movie.create({title: "Iron Man", year:"2008", plot: "In Iron Man, Tony Stark is a rich billionaire who also happens to be a genius, playboy, and philanthropist. After a weapons presentation in Iraq, terrorists manage to capture him which results in an injury that fills his chest with with shrapnel. In order to escape, he creates a suit of armor we call Iron Man."})
