db197e274f979bfc429f94b10b713d97f2b277fd
sun-shin <sunhanshin2@EMAIL.com>
2020-09-17 09:58:49 -0500

    migrations to actors and movies

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -15,6 +15,8 @@ class Api::ActorsController < ApplicationController
       first_name: params[:first_name],
       last_name: params[:last_name],
       known_for: params[:known_for]
+      gender: params[:gender],
+      age: params[:age]
     )
     @actor.save
     render "show.json.jb"
@@ -26,6 +28,8 @@ class Api::ActorsController < ApplicationController
     @actor.first_name = params[:first_name] || @actor.first_name
     @actor.last_name = params[:last_name] || @actor.last_name
     @actor.known_for = params[:known_for] || @actor.known_for
+    @actor.gender = params[:gender] || @actor.gender
+    @actor.age = params[:age] || @actor.age
     
     @actor.save
     render "show.json.jb"
---
+++ db/migrate/20200917145323_add_attributes_to_actor.rb
@@ -0,0 +1,6 @@
+class AddAttributesToActor < ActiveRecord::Migration[6.0]
+  def change
+    add_column :actors, :gender, :string
+    add_column :actors, :age, :integer
+  end
+end
