54da6c70c1c1cbb0a0a0ae2a73ce30081393e6e4
sun-shin <sunhanshin2@EMAIL.com>
2020-09-18 10:52:09 -0500

    changed english attribute(movies) to boolean

---
+++ db/migrate/20200918154903_change_englishto_boolean.rb
@@ -0,0 +1,5 @@
+class ChangeEnglishtoBoolean < ActiveRecord::Migration[6.0]
+  def change
+    change_column :movies, :english, 'boolean USING CAST(english AS boolean)'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_145323) do
+ActiveRecord::Schema.define(version: 2020_09_18_154903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_145323) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "director"
-    t.string "english"
+    t.boolean "english"
   end
 
 end
