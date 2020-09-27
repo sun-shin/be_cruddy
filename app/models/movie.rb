59d7620b37679baf28c8af4d693ac385208f2e87
sun-shin <sunhanshin2@EMAIL.com>
2020-09-26 23:12:02 -0500

    created genre & moviegenres model and updated associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,16 @@
 class Movie < ApplicationRecord
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
   
   validates :title, presence: true
   validates :year, length: { minimum: 1888 }
   validates :text, length: { in: 5..200 }
   validates :director, presence: true
   
+
+  # def genre_names
+  
+  # end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
-actor.save
+# actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
+# actor.save
 
-my_movie = Movie.create({title: "Fight Club", year:"1999", plot: "An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more."})
+# my_movie = Movie.create({title: "Fight Club", year:"1999", plot: "An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more."})
 
-my_movie = Movie.create({title: "Iron Man", year:"2008", plot: "In Iron Man, Tony Stark is a rich billionaire who also happens to be a genius, playboy, and philanthropist. After a weapons presentation in Iraq, terrorists manage to capture him which results in an injury that fills his chest with with shrapnel. In order to escape, he creates a suit of armor we call Iron Man."})
+# my_movie = Movie.create({title: "Iron Man", year:"2008", plot: "In Iron Man, Tony Stark is a rich billionaire who also happens to be a genius, playboy, and philanthropist. After a weapons presentation in Iraq, terrorists manage to capture him which results in an injury that fills his chest with with shrapnel. In order to escape, he creates a suit of armor we call Iron Man."})
+
+# Genre.create!([
+#   {name: "thriller"},
+#   {name: "comedy"},
+#   {name: "action"}
+# ])
+
+# MovieGenre.create!([
+#   {genre_id: 1, movie_id: 1},
+#   {genre_id: 2, movie_id: 2},
+#   {genre_id: 3, movie_id: 3},
+#   {genre_id: 3, movie_id: 4},
+#   {genre_id: 3, movie_id: 5},
+#   {genre_id: 2, movie_id: 6}
+# ])
